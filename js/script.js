b6892dc10cfb09810ec99f63697f52c2bd776cd3
skryu89 <mtjapwd@gmail.com>
2025-04-25 01:46:08 +0900

    commit test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
-//スティッキーヘッダー
-$(function()) {
-    var $win = $(window),
-    $fv = $('.fv'),
-    $header = $('.header'),
-    fvHeight = $fv.outerHeight();
-    fixedClass = 'fixed';
 
-      $win.on('load scroll',function()){
-        var value = $(this).scrollTop{};
-        if($win.width()) > 768) {
-            if ( value > fvHeight) {
-             $header,addClass(fixedClass);
-            }else{
-             $header.removeClass(fixedClass);
-            }
-        }
+$(function() {
+  // スライダー
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 1000,
+    cssEase: 'linear'
+  });
+
+  // スティッキーヘッダー
+  var $win = $(window);
+  var $fv = $('.fv');
+  var $section__wrapper__1 = $('.section__wrapper__1');
+  var fvHeight = $fv.outerHeight();
+  var fixedClass = 'fixed';
+
+  $win.on('load scroll', function(){
+    var value = $(this).scrollTop();
+    if ($win.width() > 768) {
+      if (value > fvHeight) {
+        $section__wrapper__1.addClass(fixedClass);
+      } else {
+        $section__wrapper__1.removeClass(fixedClass);
       }
-}
+    }
+  });
+
+  // ハンバーガーメニュー
+  $('.burger-btn').on('click',function() {
+    $('.header-nav').fadeToggle(300);
+    $(this).toggleClass('cross');
+    $('body').toggleClass('noscroll');
+  });
+});
